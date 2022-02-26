67f88335cf67f8b2a2d48988c658b13359452df9
saurabh01g <saurabh22g@gmail.com>
2022-02-26 19:32:54 +0530

    third commit by SG

--- demo.py
+++ demo.py
@@ -7,3 +7,22 @@ def mul(x,y):
     return x*y
 
 print(mul(20,10))
+
+
+# decorator to calculate execution time
+from time import time
+def mydeco(func):
+    def wrapper(n):
+        start_time = time()
+        func(n)
+        end_time = time()
+        total_time = end_time -start_time
+        print(total_time)
+    return wrapper
+
+@mydeco     #custom decorator
+def myfunc(n):
+    for i in range(n):
+        n += 1
+    
+myfunc(100000)
